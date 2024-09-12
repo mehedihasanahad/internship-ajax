ef5fbf91be9d02f28ad9afcd1fcf0862fae13a56
Ahad <ahad.egov@gmail.com>
2024-09-12 18:34:10 +0600

    updated

---
+++ AJAX/index.js
@@ -0,0 +1,47 @@
+import { consoleWithStyle } from './../helper.js';
+import './xhr.js';
+
+
+// Introduction
+
+// Read data from a web server - after the page has loaded
+// Update a web page without reloading the page
+// Send data to a web server - in the background
+
+// There is two way
+// 1) XHR
+// 2) Fetch
+
+
+// Basic API call with XHR
+
+// function loadDoc() {
+//     const xhttp = new XMLHttpRequest();
+//     xhttp.onload = function() {
+//         consoleWithStyle('XHR Response:', this.response);
+//         // document.getElementById("demo").innerHTML = this.responseText;
+//     }
+//     xhttp.open("GET", "/public/ajax_info.txt", true);
+//     xhttp.send();
+// }
+
+// loadDoc();
+
+
+// Basic API call with Fetch
+
+// function loadDocThoughFetch() {
+//     const request = new Request("/public/ajax_info.txt", {
+//         method: "GET",
+//         // body: JSON.stringify({ username: "example" }),
+//       });
+//     fetch(request)
+//     .then(async (response) => {
+//         const textResponse = await response.text();
+//         consoleWithStyle('Fetch Response:', textResponse);
+//     });
+// }
+
+// loadDocThoughFetch();
+
+
---
+++ AJAX/xhr.js
@@ -0,0 +1,33 @@
+import { consoleWithStyle } from './../helper.js';
+// The keystone of AJAX is the XMLHttpRequest object.
+
+// 1) Create an XMLHttpRequest object
+// 2) Define a callback function
+// 3) Open the XMLHttpRequest object
+// 4) Send a Request to a server
+
+
+
+// Create an XMLHttpRequest Object
+
+const XHRInstance = new XMLHttpRequest();
+consoleWithStyle('XHR instance:', XHRInstance);
+
+
+// =============================================================
+
+
+// Define a Callback Function
+
+XHRInstance.onload = function() {
+    // consoleWithStyle('Response:', this.response);
+    // What to do when the response is ready
+}
+
+
+// ===========================================================
+
+// Send a Request
+const URL = "/public/ajax_info.txt";
+XHRInstance.open("GET", URL);
+XHRInstance.send();
